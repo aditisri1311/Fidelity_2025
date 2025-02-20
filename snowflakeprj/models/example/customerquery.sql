d0f6f0c2d9471787cee4df6ddbcf7d9e2c254c28
AditiSri1311 <aditisrivastava1311@gmail.com>
2025-02-20 17:19:32 +0530

    Add files via upload

---
+++ snowflakeprj/models/example/customerquery.sql
@@ -0,0 +1,16 @@
+SELECT 
+    c.customerName, 
+    c.phone, 
+    c.city, 
+    c.state, 
+    c.country, 
+    o.orderDate, 
+    o.status, 
+    p.productName, 
+    od.quantityOrdered
+FROM FIL_DB.FIL_SCHEMA.customers c
+JOIN FIL_DB.FIL_SCHEMA.orders o ON c.customerNumber = o.customerNumber
+JOIN FIL_DB.FIL_SCHEMA.orderdetails od ON o.orderNumber = od.orderNumber
+JOIN FIL_DB.FIL_SCHEMA.products p ON od.productCode = p.productCode
+WHERE o.status = 'Pending'
+ORDER BY o.orderDate DESC
